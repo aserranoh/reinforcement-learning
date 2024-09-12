697ccb9b32a6fe24d27dd63d15cf991756773847
Antonio Serrano Hernandez <toni.serranoh@gmail.com>
2024-09-12 21:08:30 +0200

    Implemented kbandit gradient strategy and others.

--- 02-multi-arm-bandits/kbandit.py
+++ 02-multi-arm-bandits/kbandit.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 import numpy
 import random
-from typing import Callable
+from typing import Any, Callable
 from tqdm import tqdm
 
 class Problem(ABC):
@@ -45,30 +45,20 @@ class KbanditNonStationary(Problem):
         self.rewards = [reward + random.gauss(self.mean_increment, self.std_increment) for reward in self.rewards]
         return reward, int(best_action)
     
-def generate_kbandit(n: int, avg_reward: float = 0.0, std_avg_reward: float = 1.0, std_reward: float = 1.0) -> Kbandit:
+def generate_kbandit(n: int,
+                     avg_reward: float = 0.0,
+                     std_avg_reward: float = 1.0,
+                     std_reward: float = 1.0) -> Kbandit:
     average_rewards = [random.gauss(avg_reward, std_avg_reward) for i in range(n)]
     return Kbandit(average_rewards, std_reward)
 
 @dataclass
 class KbanditRun:
-    k: int
     actions: list[int] = field(init=False, default_factory=list)
     rewards: list[float] = field(init=False, default_factory=list)
     best_actions: list[int] = field(init=False, default_factory=list)
-    averages: list[float] = field(init=False)
-    actions_count: list[int] = field(init=False)
-    step_size: float | None = None
-
-    def __post_init__(self) -> None:
-        self.actions_count = [0] * self.k
-        self.averages = [0.0] * self.k
 
     def action_taken(self, action: int, reward: float, best_action: float) -> None:
-        self.actions_count[action] += 1
-        step_size = self.step_size
-        if step_size is None:
-            step_size = 1/self.actions_count[action]
-        self.averages[action] = self.averages[action] + step_size*(reward - self.averages[action])
         self.actions.append(action)
         self.rewards.append(reward)
         self.best_actions.append(int(best_action))
@@ -80,33 +70,116 @@ class KbanditRun:
         is_best_action = numpy.array(self.actions) == numpy.array(self.best_actions)
         return numpy.cumsum(is_best_action)/numpy.arange(1, len(self.actions) + 1)
 
+class ActionStrategy(ABC):
+
+    @abstractmethod
+    def select_next_action(self) -> int:
+        pass
+
+    @abstractmethod
+    def action_taken(self, action: int, reward: float) -> None:
+        pass
+
+@dataclass
+class SampleAveragesStrategy(ActionStrategy):
+    k: int
+    averages: list[float] = field(init=False, repr=False)
+    actions_count: list[int] = field(init=False, repr=False)
+    step_size: float | None = None
+    initial_estimates: list[float] | None = None
+    t: int = field(init=False, default=1, repr=False)
+    
+    def __post_init__(self) -> None:
+        self.actions_count = [0] * self.k
+        if self.initial_estimates is not None:
+            self.averages = [x for x in self.initial_estimates]
+        else:
+            self.averages = [0.0] * self.k
+
+    def action_taken(self, action: int, reward: float) -> None:
+        self.actions_count[action] += 1
+        step_size = self.step_size
+        if step_size is None:
+            step_size = 1/self.actions_count[action]
+        self.averages[action] = self.averages[action] + step_size*(reward - self.averages[action])
+        self.t += 1
+
+@dataclass
+class EpsilonStrategy(SampleAveragesStrategy):
+    epsilon: float = 0.0
+
+    def select_next_action(self) -> int:
+        dice = random.random()
+        if dice > self.epsilon:
+            action = numpy.argmax(self.averages)
+        else:
+            action = random.randrange(self.k)
+        return int(action)
+
+@dataclass
+class UCBStrategy(SampleAveragesStrategy):
+    c: float = 0.0
+
+    def select_next_action(self) -> int:
+        try:
+            action = self.actions_count.index(0)
+        except ValueError:
+            action = numpy.argmax(numpy.array(self.averages) + self.c * numpy.log(self.t) / numpy.array(self.actions_count))
+        return int(action)
+
+@dataclass
+class GradientStrategy(ActionStrategy):
+    k: int
+    step_size: float
+    baseline: float | None = None
+    h: Any = field(init=False)
+    pr: Any = field(init=False)
+    average_reward: float = field(init=False, default=0.0)
+    count: int = field(init=False, default=0)
+
+    def __post_init__(self) -> None:
+        self.h = numpy.zeros(self.k)
+        self.pr = numpy.exp(self.h) / sum(numpy.exp(self.h))
+
+    def select_next_action(self) -> int:
+        return numpy.random.choice(self.k, p=self.pr)
+
+    def action_taken(self, action: int, reward: float) -> None:
+        baseline = self.baseline
+        if baseline is None:
+            baseline = self.average_reward
+            
+        for i in range(self.k):
+            error = reward - baseline
+            if i == action:
+                self.h[i] += self.step_size * error * (1 - self.pr[i])
+            else:
+                self.h[i] -= self.step_size * error * self.pr[i]
+
+        self.pr = numpy.exp(self.h) / sum(numpy.exp(self.h))
+        
+        self.count += 1
+        self.average_reward += (1/self.count) * (reward - self.average_reward)
+
 @dataclass
 class Runner:
     steps: int
-    epsilon: float = 0.0
-    step_size: float | None = None
+    action_strategy: ActionStrategy
     
     def run(self, problem: Problem) -> KbanditRun:
-        result = KbanditRun(len(problem), step_size=self.step_size)
-
+        result = KbanditRun()
         for _ in range(self.steps):
-            dice = random.random()
-
-            if dice > self.epsilon:
-                action = numpy.argmax(result.averages)
-            else:
-                action = random.randrange(len(problem))
-            reward, best_action = problem.get_reward(int(action))
-
-            result.action_taken(int(action), reward, best_action)
+            action = self.action_strategy.select_next_action()
+            reward, best_action = problem.get_reward(action)
+            self.action_strategy.action_taken(action, reward)
+            result.action_taken(action, reward, best_action)
         return result
 
-def run_problem(steps: int, n: int, epsilon: float, problem_factory: Callable, step_size: float | None = None) -> tuple:
-    print(f"n: {n}, steps: {steps}, epsilon: {epsilon}")
-    problems = [problem_factory() for _ in range(n)]
+def run_problem(steps: int, n: int, problem_factory: Callable, strategy_factory: Callable) -> tuple:
+    print(f"n: {n}, steps: {steps}")
     results = []
-    for problem, _ in zip(problems, tqdm(range(n))):
-        results.append(Runner(steps=steps, epsilon=epsilon, step_size=step_size).run(problem))
+    for _ in tqdm(range(n)):
+        results.append(Runner(steps=steps, action_strategy=strategy_factory()).run(problem_factory()))
 
     average_reward = numpy.stack([result.average_reward() for result in results])
     average_reward = numpy.average(average_reward, axis=0)
